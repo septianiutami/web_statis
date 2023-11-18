7dd401d66cf0e8f0d6987052705692c5a2e624c1
septianiutami <141755706+septianiutami@users.noreply.github.com>
2023-11-18 16:32:35 +0700

    Delete js/UTS.js

--- js/UTS.js
+++
@@ -1,8 +0,0 @@
-alert ('Halo! Para Warganet')
-
-const menu = document.querySelector('.navbar #menu');
-const nav = document.querySelector('.navbar .navbar-nav');
-
-menu.addEventListener('click', function() {
-    nav.classList.toggle('active')
-});
